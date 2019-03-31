89878ff7c55dd8c09095108344216604efe74a74
Rodrigo Silva Pinto <rsp.4321@gmail.com>
2019-03-31 18:24:17 -0300

    Foi implementada a rotina de abertura em arquivo no formulário do aluno ("FrCadAluno.btnAlunoActionPerformed"). Para abstrair o diretório do usuário no sistema operacional, foi utilizada a classe java.nio.file.Path e o método System.getProperty("user.home") para obter o diretório adequado independentemente do sitema operacional utilizado. Por padrão, está salvando na pasta "OO_Aula5". O nome do arquivo, por enquanto, pode ser configurado na constante NOME_ARQUIVO, dentro da classe do frame.

--- src/gui/FrCadAluno2.java
+++ src/gui/FrCadAluno2.java
@@ -3,8 +3,16 @@ package gui;
 
 //import classes.*;
 import academico.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class FrCadAluno2 extends javax.swing.JFrame {
@@ -12,6 +20,7 @@ public class FrCadAluno2 extends javax.swing.JFrame {
     private ArrayList<Aluno> lista;
     private int newEdit; //0: Novo , 1: Editar
     private int indexEdit;
+    private final String NOME_ARQUIVO = "registro.txt";
 
     public FrCadAluno2() {
         initComponents();
@@ -152,9 +161,9 @@ public class FrCadAluno2 extends javax.swing.JFrame {
         edtMatricula = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         edtResultado = new javax.swing.JTextArea();
-        lblAnoIngresso = new javax.swing.JLabel();
-        edtAnoIngresso = new javax.swing.JTextField();
         cmbSexo = new javax.swing.JComboBox<>();
+        btnAbrirArquivo = new javax.swing.JButton();
+        btnSalvarArquivo = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -232,20 +241,21 @@ public class FrCadAluno2 extends javax.swing.JFrame {
         edtResultado.setRows(5);
         jScrollPane1.setViewportView(edtResultado);
 
-        lblAnoIngresso.setText("Ano de ingresso");
-
-        edtAnoIngresso.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyTyped(java.awt.event.KeyEvent evt) {
-                edtAnoIngressoKeyTyped(evt);
-            }
-        });
-
         cmbSexo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "M", "F" }));
         cmbSexo.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 cmbSexoKeyTyped(evt);
             }
         });
+
+        btnAbrirArquivo.setText("Abrir do arquivo");
+        btnAbrirArquivo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAbrirArquivoActionPerformed(evt);
+            }
+        });
+
+        btnSalvarArquivo.setText("Salvar no arquivo");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -256,38 +266,37 @@ public class FrCadAluno2 extends javax.swing.JFrame {
                 .addGap(22, 22, 22)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnNovo)
+                        .addComponent(lblMatricula)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnEditar)
+                        .addComponent(edtMatricula, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(lblSexo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cmbSexo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(27, 27, 27)
+                        .addComponent(lblIdade)
+                        .addGap(10, 10, 10)
+                        .addComponent(edtIdade, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(lblNome)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(edtNome, javax.swing.GroupLayout.PREFERRED_SIZE, 546, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnNovo)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnEditar))
+                            .addComponent(btnAbrirArquivo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(btnSalvar)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnCancelar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnExcluir))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lblMatricula)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(1, 1, 1)
-                                .addComponent(lblNome)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(edtMatricula, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(lblSexo)
+                                .addComponent(btnCancelar)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(cmbSexo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(27, 27, 27)
-                                .addComponent(lblIdade)
-                                .addGap(10, 10, 10)
-                                .addComponent(edtIdade, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(lblAnoIngresso)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(edtAnoIngresso, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(edtNome))))
+                                .addComponent(btnExcluir))
+                            .addComponent(btnSalvarArquivo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addGap(0, 17, Short.MAX_VALUE))
             .addComponent(jScrollPane1)
         );
@@ -303,7 +312,11 @@ public class FrCadAluno2 extends javax.swing.JFrame {
                     .addComponent(btnSalvar, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                     .addComponent(btnEditar, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                     .addComponent(btnNovo, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnAbrirArquivo)
+                    .addComponent(btnSalvarArquivo))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -318,10 +331,8 @@ public class FrCadAluno2 extends javax.swing.JFrame {
                             .addComponent(lblSexo)
                             .addComponent(lblIdade)
                             .addComponent(lblMatricula)
-                            .addComponent(lblAnoIngresso)
-                            .addComponent(edtAnoIngresso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(cmbSexo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
@@ -337,10 +348,10 @@ public class FrCadAluno2 extends javax.swing.JFrame {
     }//GEN-LAST:event_btnCancelarActionPerformed
     private void btnSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalvarActionPerformed
         Aluno a = this.copiarCamposGUI();
-        
+
         if (newEdit == 0) {
             this.lista.add(a);
-        
+
         } else if (newEdit == 1) {
             Aluno b = this.lista.get(this.indexEdit);
             b.setNome(a.getNome());
@@ -348,7 +359,7 @@ public class FrCadAluno2 extends javax.swing.JFrame {
             b.setSexo(a.getSexo());
             b.setIdade(a.getIdade());
         }
-        
+
         edtResultado.setText(this.imprimirListaCompleta());
         this.imprimirListaCompleta();
         resetFields(false);
@@ -386,12 +397,6 @@ public class FrCadAluno2 extends javax.swing.JFrame {
         edtResultado.setText(this.imprimirListaCompleta());
     }//GEN-LAST:event_btnExcluirActionPerformed
 
-    private void edtAnoIngressoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_edtAnoIngressoKeyTyped
-        if (evt.getKeyChar() == java.awt.event.KeyEvent.VK_ENTER) {
-            btnSalvar.requestFocus();
-        }
-    }//GEN-LAST:event_edtAnoIngressoKeyTyped
-
     private void cmbSexoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_cmbSexoKeyTyped
         if (evt.getKeyChar() == java.awt.event.KeyEvent.VK_ENTER) {
             edtIdade.requestFocus();
@@ -401,25 +406,136 @@ public class FrCadAluno2 extends javax.swing.JFrame {
     private void edtIdadeKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_edtIdadeKeyTyped
 
         if (evt.getKeyChar() == java.awt.event.KeyEvent.VK_ENTER) {
-            edtAnoIngresso.requestFocus();
+            btnSalvar.requestFocus();
         }
 
     }//GEN-LAST:event_edtIdadeKeyTyped
 
+    private void btnAbrirArquivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAbrirArquivoActionPerformed
+
+        // Como a equipe está desenvolvendo tanto no Windows quanto no linux,
+        // vou utilizar a classe java.nio.file.Path para abstrair os diretórios
+        // sem precisar utlizar métodos de detecção do sistema operacional por enquanto
+        Path caminho_arquivo = Paths.get( // O objeto "Paths" constrói o objeto "Path" conforme as "partes" do diretório dadas
+                System.getProperty("user.home"), // a rotina System.getProperty() retorna algumas variáveis de ambiente definidas pela JVM no momento da execução. A veriável "user.home" retorna o diretório do usuário no sistema operacional
+                "OO_Aula5", // Essa "parte" indica que se trata da pasta "OO_Aula5"
+//                "registro.txt");                    // Essa "parte" indica o arquivo
+                NOME_ARQUIVO);
+
+        FileReader file_reader_arquivo = null;
+        Scanner sc_arquivo = null;
+
+        ArrayList<Aluno> lstAluno = new ArrayList<>();
+
+        try {
+
+
+            // Usando o método Path.toString() para retornar o diretório no formato de string
+            /*FileReader */ file_reader_arquivo = new FileReader(caminho_arquivo.toString());
+
+            /*Scanner */ sc_arquivo = new Scanner(file_reader_arquivo);  // Criando um objeto "Scanner" para ler do objeto "FileReader"
+
+            // Como o formato de arquivo escolhido é o CSV, vamos alterar o delimitador para o caractere ";"
+            sc_arquivo.useDelimiter(";");
+
+            // Por questão de ambiguidade da IDE e do compilador, fiz a inicialização manual das variáveis
+            int matricula = 0, idade = -1;
+            String nome = "";
+            char sexo = 'M';
+
+            // Ordem de colunas: Matricula -> Nome -> Idade -> Sexo
+            // A estratégia é percorrer os campos até não haver mais nenhum para ler (informado pelo método sc_Arquivo.hasNext() )
+            // Para tal, utilizaremos um while com uma variável de controle "campo" e um contador "i"
+            // ela indicará qual o tipo de campo que é
+            // Para fazer isso, faremos uma matemática básica
+            // Como são 4 campos, se implementarmos um contador de campos, observaremos um certo padrão:
+            // 1) O campo de matrícula aparecerá quando aparecer resto 0 numa divisão de i por 4;
+            // 2) O campo de nome aparecerá quando aparecer resto 1 numa divisão de i por 4;
+            // 3) O campo de idade aparecerá quando aparecer resto 2 numa divisão de i por 4;
+            // 4) O campo de sexo aparecerá quando aparecer resto 3 numa divisão de i por 4.
+            // A partir desses padrões, faremos a identificação do campo lido e colocaremos no vetor lstAluno
+            int campo = 0, i = 0;
+            final int CAMPO_MATRICULA = 0;  // a cláusula "final" indica que se trata de uma constante
+            final int CAMPO_NOME = 1;
+            final int CAMPO_IDADE = 2;
+            final int CAMPO_SEXO = 3;
+
+            while (sc_arquivo.hasNext()) {
+
+                campo = i % 4; // Obtendo o resto da divisão de i por 4
+
+                switch (campo) {
+
+                    case CAMPO_MATRICULA:
+                        matricula = sc_arquivo.nextInt();
+                        break;
+
+                    case CAMPO_NOME:
+                        nome = sc_arquivo.next();
+                        break;
+
+                    case CAMPO_IDADE:
+                        idade = sc_arquivo.nextInt();
+                        break;
+
+                    case CAMPO_SEXO:
+
+                        // Como o sexo é o último campo, vamos instanciar o objeto e armazenar na lista
+                        sexo = sc_arquivo.next().charAt(0); // Por se tratar de um char, tem que retirar apenas o primeiro caracter
+
+                        Aluno a = new Aluno(nome, sexo, idade, matricula);  // Instanciando o aluno
+                        lstAluno.add(a);
+                        break;
+                }
+                
+                i++;    // Incrementando o contador para poder fazer a divisão
+            }
+            
+            file_reader_arquivo.close();
+
+            // Código gerado automaticamente pelo NetBeans
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FrCadAluno2.class.getName()).log(Level.SEVERE, null, ex);  // Esse método só faz o log do erro
+
+            // Como o método FileReader.close() dispara uma exceção diferente ("IOException"), 
+            // coloquei o método genérico para a classe "IOException"
+        } catch (IOException e) {
+            Logger.getLogger(FrCadAluno2.class.getName()).log(Level.SEVERE, null, e);
+
+        } finally { // A cláusula "finally" é recomendada pelo Java para finalizar os objetos ligados a recursos utilizados
+
+            if (sc_arquivo != null) {
+                sc_arquivo.close();
+            }
+
+            /*
+            try {
+                file_reader_arquivo.close();
+            } catch (IOException ex) {
+                Logger.getLogger(FrCadAluno2.class.getName()).log(Level.SEVERE, null, ex);
+            }
+             */
+        }
+        
+        this.lista = lstAluno;
+        
+        JOptionPane.showMessageDialog(this, "Arquivo '"+ caminho_arquivo.toString() +"' carregado com sucesso!", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+    }//GEN-LAST:event_btnAbrirArquivoActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAbrirArquivo;
     private javax.swing.JButton btnCancelar;
     private javax.swing.JButton btnEditar;
     private javax.swing.JButton btnExcluir;
     private javax.swing.JButton btnNovo;
     private javax.swing.JButton btnSalvar;
+    private javax.swing.JButton btnSalvarArquivo;
     private javax.swing.JComboBox<String> cmbSexo;
-    private javax.swing.JTextField edtAnoIngresso;
     private javax.swing.JTextField edtIdade;
     private javax.swing.JTextField edtMatricula;
     private javax.swing.JTextField edtNome;
     private javax.swing.JTextArea edtResultado;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JLabel lblAnoIngresso;
     private javax.swing.JLabel lblIdade;
     private javax.swing.JLabel lblMatricula;
     private javax.swing.JLabel lblNome;
